048c5673040c8348a2ee8d0f12eb68b92d3d6289
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2017-04-09 15:26:12 +0200

    ~ doesn't emit duplicate node creations anymore

--- src/main/java/com/kasisoft/mgnl/versionhandler/DescriptiveProducer.java
+++ src/main/java/com/kasisoft/mgnl/versionhandler/DescriptiveProducer.java
@@ -11,6 +11,9 @@ import lombok.*;
 /**
  * This producer generates a JCR based tree structure. 
  * 
+ * @todo [09-Apr-2017:KASI]   Base the description process upon the {@link MapProducer} as altering/extensions
+ *                            to the tree is somewhat complicated using a {@link StringFBuilder}.
+ * 
  * @author daniel.kasmeroglu@kasisoft.net
  */
 @FieldDefaults(level = AccessLevel.PRIVATE)
@@ -43,7 +46,10 @@ public class DescriptiveProducer implements Producer<StringFBuilder> {
     } else if( parentPath.endsWith("/") ) {
       parentPath = parentPath.substring( 0, parentPath.length() - 1 );
     }
-    parent.appendF( "(new) %s/%s%s\n", parentPath, name, nodeTypes ? ("[" + nodeType + "]") : "" );
+    String line = String.format( "(new) %s/%s%s\n", parentPath, name, nodeTypes ? ("[" + nodeType + "]") : "" );
+    if( parent.indexOf( line ) == -1 ) {
+      parent.appendF( line );
+    }
     return parent;
   }
 
