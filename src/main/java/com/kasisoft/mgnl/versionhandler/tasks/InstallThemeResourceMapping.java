51f8a348526f1ac92a76689e5ecba769d0b7c012
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2017-05-17 21:37:00 +0200

    + added the possibility to configure the resource folder names

--- src/main/java/com/kasisoft/mgnl/versionhandler/tasks/InstallThemeResourceMapping.java
+++ src/main/java/com/kasisoft/mgnl/versionhandler/tasks/InstallThemeResourceMapping.java
@@ -21,18 +21,39 @@ public class InstallThemeResourceMapping implements TreeBuilderProvider {
   String    themeName;
   String    moduleName;
   boolean   includeTheme;
+  String    jsFolderName;
+  String    cssFolderName;
+  String    fontsFolderName;
   
   public InstallThemeResourceMapping( @Nonnull String module, @Nonnull String theme ) {
-    moduleName    = module;
-    themeName     = theme;
-    includeTheme  = false;
+    moduleName      = module;
+    themeName       = theme;
+    includeTheme    = false;
+    jsFolderName    = "js";
+    cssFolderName   = "css";
+    fontsFolderName = "fonts";
   }
 
   public InstallThemeResourceMapping includeThemeName() {
     includeTheme = true;
     return this;
   }
-  
+
+  public InstallThemeResourceMapping javascriptFolderName( @Nonnull String name ) {
+    jsFolderName = name;
+    return this;
+  }
+
+  public InstallThemeResourceMapping cssFolderName( @Nonnull String name ) {
+    cssFolderName = name;
+    return this;
+  }
+
+  public InstallThemeResourceMapping fontsFolderName( @Nonnull String name ) {
+    fontsFolderName = name;
+    return this;
+  }
+
   @Override
   public String getDescription() {
     return desc_install_theme.format( themeName, moduleName );
@@ -42,27 +63,35 @@ public class InstallThemeResourceMapping implements TreeBuilderProvider {
   public TreeBuilder create() {
     String prefix = includeTheme ? "/" + themeName : "";
     return new TreeBuilder()
-      .substitution( "theme", themeName )
-      .substitution( "module", moduleName )
+      
+      .substitution( "theme"       , themeName       )
+      .substitution( "module"      , moduleName      )
+      .substitution( "jsFolder"    , jsFolderName    )
+      .substitution( "cssFolder"   , cssFolderName   )
+      .substitution( "fontsFolder" , fontsFolderName )
+      
       .sContentNode( "modules/${module}/virtualURIMapping/cssFiles" )
         .clazz( RegexpVirtualURIMapping.class )
-        .propertyF( "fromURI" , "^%s/css/(.+)$", prefix )
-        .property( "toURI"   , "forward:/.resources/${theme}/css/$1" )
+        .propertyF( "fromURI" , "^%s/${cssFolder}/(.+)$", prefix )
+        .property( "toURI"   , "forward:/.resources/${theme}/${cssFolder}/$1" )
       .sEnd()
+      
       .sContentNode( "modules/${module}/virtualURIMapping/fontFiles" )
         .clazz( RegexpVirtualURIMapping.class )
-        .propertyF( "fromURI" , "^%s/fonts/(.+)$", prefix )
-        .property( "toURI"   , "forward:/.resources/${theme}/fonts/$1" )
+        .propertyF( "fromURI" , "^%s/${fontsFolder}/(.+)$", prefix )
+        .property( "toURI"   , "forward:/.resources/${theme}/${fontsFolder}/$1" )
       .sEnd()
+      
       .sContentNode( "modules/${module}/virtualURIMapping/iconFiles" )
         .clazz( RegexpVirtualURIMapping.class )
         .propertyF( "fromURI" , "^%s/icons/(.+)$", prefix )
         .property( "toURI"   , "forward:/.resources/${theme}/icons/$1" )
       .sEnd()
+      
       .sContentNode( "modules/${module}/virtualURIMapping/jsFiles" )
         .clazz( RegexpVirtualURIMapping.class )
-        .propertyF( "fromURI" , "^%s/js/(.+)$", prefix )
-        .property( "toURI"   , "forward:/.resources/${theme}/js/$1" )
+        .propertyF( "fromURI" , "^%s/${jsFolder}/(.+)$", prefix )
+        .property( "toURI"   , "forward:/.resources/${theme}/${jsFolder}/$1" )
       .sEnd()
       ;
   }
--- src/test/java/com/kasisoft/mgnl/versionhandler/tasks/InstallThemeResourceMappingTest.java
+++ src/test/java/com/kasisoft/mgnl/versionhandler/tasks/InstallThemeResourceMappingTest.java
@@ -84,6 +84,105 @@ public class InstallThemeResourceMappingTest extends AbstractTreeBuilderProvider
   }
 
   @Test
+  public void cssFolderName() {
+    
+    InstallThemeResourceMapping tb = new InstallThemeResourceMapping( "dodod-module", "dodo-theme" )
+      .cssFolderName( "bibo-css" );
+    
+    String expected = ""
+      + "(new) /modules[mgnl:contentNode]\n"
+      + "(new) /modules/dodod-module[mgnl:contentNode]\n"
+      + "(new) /modules/dodod-module/virtualURIMapping[mgnl:contentNode]\n"
+      + "(new) /modules/dodod-module/virtualURIMapping/cssFiles[mgnl:contentNode]\n"
+      + "@fromURI = '^/bibo-css/(.+)$'\n"
+      + "@toURI = 'forward:/.resources/dodo-theme/bibo-css/$1'\n"
+      + "@class = 'info.magnolia.cms.beans.config.RegexpVirtualURIMapping'\n"
+      + "(new) /modules/dodod-module/virtualURIMapping/fontFiles[mgnl:contentNode]\n"
+      + "@fromURI = '^/fonts/(.+)$'\n"
+      + "@toURI = 'forward:/.resources/dodo-theme/fonts/$1'\n"
+      + "@class = 'info.magnolia.cms.beans.config.RegexpVirtualURIMapping'\n"
+      + "(new) /modules/dodod-module/virtualURIMapping/iconFiles[mgnl:contentNode]\n"
+      + "@fromURI = '^/icons/(.+)$'\n"
+      + "@toURI = 'forward:/.resources/dodo-theme/icons/$1'\n"
+      + "@class = 'info.magnolia.cms.beans.config.RegexpVirtualURIMapping'\n"
+      + "(new) /modules/dodod-module/virtualURIMapping/jsFiles[mgnl:contentNode]\n"
+      + "@fromURI = '^/js/(.+)$'\n"
+      + "@toURI = 'forward:/.resources/dodo-theme/js/$1'\n"
+      + "@class = 'info.magnolia.cms.beans.config.RegexpVirtualURIMapping'\n"
+      ;
+    
+    String desc = buildDescription( tb );
+    assertThat( desc, is( expected ) );
+    
+  }
+
+  @Test
+  public void jsFolderName() {
+    
+    InstallThemeResourceMapping tb = new InstallThemeResourceMapping( "dodod-module", "dodo-theme" )
+      .javascriptFolderName( "bibo-js" );
+    
+    String expected = ""
+      + "(new) /modules[mgnl:contentNode]\n"
+      + "(new) /modules/dodod-module[mgnl:contentNode]\n"
+      + "(new) /modules/dodod-module/virtualURIMapping[mgnl:contentNode]\n"
+      + "(new) /modules/dodod-module/virtualURIMapping/cssFiles[mgnl:contentNode]\n"
+      + "@fromURI = '^/css/(.+)$'\n"
+      + "@toURI = 'forward:/.resources/dodo-theme/css/$1'\n"
+      + "@class = 'info.magnolia.cms.beans.config.RegexpVirtualURIMapping'\n"
+      + "(new) /modules/dodod-module/virtualURIMapping/fontFiles[mgnl:contentNode]\n"
+      + "@fromURI = '^/fonts/(.+)$'\n"
+      + "@toURI = 'forward:/.resources/dodo-theme/fonts/$1'\n"
+      + "@class = 'info.magnolia.cms.beans.config.RegexpVirtualURIMapping'\n"
+      + "(new) /modules/dodod-module/virtualURIMapping/iconFiles[mgnl:contentNode]\n"
+      + "@fromURI = '^/icons/(.+)$'\n"
+      + "@toURI = 'forward:/.resources/dodo-theme/icons/$1'\n"
+      + "@class = 'info.magnolia.cms.beans.config.RegexpVirtualURIMapping'\n"
+      + "(new) /modules/dodod-module/virtualURIMapping/jsFiles[mgnl:contentNode]\n"
+      + "@fromURI = '^/bibo-js/(.+)$'\n"
+      + "@toURI = 'forward:/.resources/dodo-theme/bibo-js/$1'\n"
+      + "@class = 'info.magnolia.cms.beans.config.RegexpVirtualURIMapping'\n"
+      ;
+    
+    String desc = buildDescription( tb );
+    assertThat( desc, is( expected ) );
+    
+  }
+
+  @Test
+  public void fontsFolderName() {
+    
+    InstallThemeResourceMapping tb = new InstallThemeResourceMapping( "dodod-module", "dodo-theme" )
+      .fontsFolderName( "bibo-fonts" );
+    
+    String expected = ""
+      + "(new) /modules[mgnl:contentNode]\n"
+      + "(new) /modules/dodod-module[mgnl:contentNode]\n"
+      + "(new) /modules/dodod-module/virtualURIMapping[mgnl:contentNode]\n"
+      + "(new) /modules/dodod-module/virtualURIMapping/cssFiles[mgnl:contentNode]\n"
+      + "@fromURI = '^/css/(.+)$'\n"
+      + "@toURI = 'forward:/.resources/dodo-theme/css/$1'\n"
+      + "@class = 'info.magnolia.cms.beans.config.RegexpVirtualURIMapping'\n"
+      + "(new) /modules/dodod-module/virtualURIMapping/fontFiles[mgnl:contentNode]\n"
+      + "@fromURI = '^/bibo-fonts/(.+)$'\n"
+      + "@toURI = 'forward:/.resources/dodo-theme/bibo-fonts/$1'\n"
+      + "@class = 'info.magnolia.cms.beans.config.RegexpVirtualURIMapping'\n"
+      + "(new) /modules/dodod-module/virtualURIMapping/iconFiles[mgnl:contentNode]\n"
+      + "@fromURI = '^/icons/(.+)$'\n"
+      + "@toURI = 'forward:/.resources/dodo-theme/icons/$1'\n"
+      + "@class = 'info.magnolia.cms.beans.config.RegexpVirtualURIMapping'\n"
+      + "(new) /modules/dodod-module/virtualURIMapping/jsFiles[mgnl:contentNode]\n"
+      + "@fromURI = '^/js/(.+)$'\n"
+      + "@toURI = 'forward:/.resources/dodo-theme/js/$1'\n"
+      + "@class = 'info.magnolia.cms.beans.config.RegexpVirtualURIMapping'\n"
+      ;
+    
+    String desc = buildDescription( tb );
+    assertThat( desc, is( expected ) );
+    
+  }
+
+  @Test
   public void hasTitle() {
     InstallThemeResourceMapping tb = new InstallThemeResourceMapping( "dodod-module", "dodo-theme" );
     assertNotNull( StringFunctions.cleanup( tb.getTitle() ) );
